5d56b49a290f0070833d46702649c478e4dd9650
plotsklapps <plotsklapps@gmail.com>
2023-05-29 15:17:29 +0000

    - Decided to go another route for responsiveness; - Satisfied VGA linter.

--- lib/mobile/educationscreen_mobile.dart
+++ lib/mobile/educationscreen_mobile.dart
@@ -12,7 +12,6 @@ class EducationScreenMobile extends StatefulWidget {
 class EducationScreenMobileState extends State<EducationScreenMobile> {
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -21,23 +20,17 @@ class EducationScreenMobileState extends State<EducationScreenMobile> {
         ),
         body: SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsets.all(Responsive.isMobile(context) ? 16 : 20),
+            padding: const EdgeInsets.all(20),
             child: Column(
               children: [
                 Row(
-                  mainAxisAlignment: Responsive.isMobile(context)
-                      ? MainAxisAlignment.center
-                      : MainAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     FlipCard(
                       front: Card(
                         child: SizedBox(
-                          // TODO(CapturedHeart): or Use the responsive
-                          //  constructor this way.
-                          width: Responsive.isMobile(context)
-                              ? size.width * 0.8
-                              : size.width * 0.4,
-                          height: size.height * 0.2,
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          height: MediaQuery.of(context).size.height * 0.2,
                           child: const ListTile(
                             leading: Icon(FontAwesomeIcons.graduationCap),
                             title: Text('Flutter Bootcamp 2021'),
@@ -47,13 +40,8 @@ class EducationScreenMobileState extends State<EducationScreenMobile> {
                       ),
                       back: Card(
                         child: SizedBox(
-                          // TODO(CapturedHeart): or Use the responsive
-                          //  constructor this way
-
-                          width: Responsive.isMobile(context)
-                              ? size.width * 0.8
-                              : size.width * 0.4,
-                          height: size.height * 0.2,
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          height: MediaQuery.of(context).size.height * 0.2,
                           child: Image.asset('assets/flutterbootcamp2021.jpg'),
                         ),
                       ),
@@ -67,8 +55,8 @@ class EducationScreenMobileState extends State<EducationScreenMobile> {
                     FlipCard(
                       front: Card(
                         child: SizedBox(
-                          width: size.width * 0.8,
-                          height: size.height * 0.2,
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          height: MediaQuery.of(context).size.height * 0.2,
                           child: const ListTile(
                             leading: Icon(FontAwesomeIcons.graduationCap),
                             title: Text('Dart from Novice to Expert 2022'),
@@ -78,8 +66,8 @@ class EducationScreenMobileState extends State<EducationScreenMobile> {
                       ),
                       back: Card(
                         child: SizedBox(
-                          width: size.width * 0.8,
-                          height: size.height * 0.2,
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          height: MediaQuery.of(context).size.height * 0.2,
                           child:
                               Image.asset('assets/dartnovicetoexpert2022.jpg'),
                         ),
