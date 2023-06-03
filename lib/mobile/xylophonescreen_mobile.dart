f6a309351cf4aa8d32b4ad34f20ec6d6336b18d5
plotsklapps <plotsklapps@gmail.com>
2023-06-03 10:44:44 +0200

    - Refactored xylophonescreen_mobile.dart to satisfy linter.

--- lib/mobile/xylophonescreen_mobile.dart
+++ lib/mobile/xylophonescreen_mobile.dart
@@ -4,15 +4,16 @@ class XylophoneScreenMobile extends StatelessWidget {
   const XylophoneScreenMobile({super.key});
 
   void playSound(int soundNumber) {
-    final audioPlayer = AudioPlayer();
-    audioPlayer.setAsset('assets/sounds/note$soundNumber.wav');
-    audioPlayer.play();
+    // Use the just_audio package to first load the sound and then play it.
+    AudioPlayer()
+      ..setAsset('assets/sounds/xylophone_note$soundNumber.wav')
+      ..play();
   }
 
-  Expanded buildKey({required Color color, required int soundNumber}) {
+  Expanded buildNotekey({required Color color, required int soundNumber}) {
     return Expanded(
       child: Padding(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(8),
         child: MaterialButton(
           color: color,
           onPressed: () {
@@ -27,17 +28,21 @@ class XylophoneScreenMobile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text('Xylophone'),
+        centerTitle: true,
+      ),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            buildKey(color: Colors.red, soundNumber: 1),
-            buildKey(color: Colors.orange, soundNumber: 2),
-            buildKey(color: Colors.yellow, soundNumber: 3),
-            buildKey(color: Colors.green, soundNumber: 4),
-            buildKey(color: Colors.teal, soundNumber: 5),
-            buildKey(color: Colors.blue, soundNumber: 6),
-            buildKey(color: Colors.purple, soundNumber: 7),
+            buildNotekey(color: Colors.red, soundNumber: 1),
+            buildNotekey(color: Colors.orange, soundNumber: 2),
+            buildNotekey(color: Colors.yellow, soundNumber: 3),
+            buildNotekey(color: Colors.green, soundNumber: 4),
+            buildNotekey(color: Colors.teal, soundNumber: 5),
+            buildNotekey(color: Colors.blue, soundNumber: 6),
+            buildNotekey(color: Colors.purple, soundNumber: 7),
           ],
         ),
       ),
