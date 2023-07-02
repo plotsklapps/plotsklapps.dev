4c289d6c654369d5ec6750e9c20f56bad8a5d04a
plotsklapps <plotsklapps@gmail.com>
2023-07-02 15:43:37 +0000

    - Extracted the navigationbar code to it's own file.

--- lib/components/navigation_bar.dart
+++ lib/components/navigation_bar.dart
@@ -1,9 +1,20 @@
 import 'package:portfolio/all_imports.dart';
 
-NavigationBar buildNavigationBar(WidgetRef ref) {
+NavigationBar buildNavigationBar(WidgetRef ref, PageController pageController) {
   return NavigationBar(
     onDestinationSelected: (int index) async {
+      // This is where we needed the PageController for. First, we
+      // set the new page index to the Riverpod Provider.
+      // Second, we use that new index to navigate to the
+      // corresponding widget in the PageView with a nice animation.
       ref.read(currentPageIndexProvider.notifier).state = index;
+      await pageController.animateToPage(
+        ref.watch(currentPageIndexProvider),
+        duration: const Duration(
+          milliseconds: 1000,
+        ),
+        curve: Curves.bounceOut,
+      );
     },
     // The currentIndex is read from the pageIndexProvider.
     selectedIndex: ref.watch(currentPageIndexProvider),
